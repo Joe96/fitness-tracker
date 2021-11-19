d443056a7a97573ac75f4c541dc83591b3d8d306
Joel Afamefune <jafamefune@gmail.com>
2021-11-19 11:12:14 -0600

    fixed exercise route in homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 router.get('/exercise', (req,res) => {
     console.log("Exercise route hit")
-    res.sendFile(path.join(__dirname, "../public/stats.html"))
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
 });
 
 router.get('/stats', (req,res) =>{
